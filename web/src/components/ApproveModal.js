73fdfd0251ce17a639b029f71877b4796b883fa3
Fabian Reinders <fabian@fabiancdng.com>
2021-05-25 17:04:28 +0200

    fix name input on approve modal

--- web/src/components/ApproveModal.js
+++ web/src/components/ApproveModal.js
@@ -1,8 +1,11 @@
 import { ArrowForwardIcon } from '@chakra-ui/icons'
 import { Button, FormControl, FormLabel, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure } from '@chakra-ui/react'
+import { useState } from 'react'
 
 const ApproveModal = ({ isApplication, application }) => {
     const { isOpen, onOpen, onClose } = useDisclosure()
+
+    const [name, setName] = useState(isApplication ? application.name : application.team.name)
   
     return (
       <>
@@ -11,18 +14,25 @@ const ApproveModal = ({ isApplication, application }) => {
         <Modal isOpen={isOpen} onClose={onClose}>
           <ModalOverlay />
           <ModalContent>
-            <ModalHeader>{ isApplication ? 'Anmeldung' : 'Team ' } annehmen/bearbeiten</ModalHeader>
+            <ModalHeader>{ isApplication ? 'Anmeldung' : 'Team ' } annehmen / bearbeiten</ModalHeader>
             <ModalCloseButton />
             <ModalBody pb={6}>
               <FormControl>
                 <FormLabel>Name</FormLabel>
-                <Input value={isApplication ? application.name : application.team.name} />
+                <Input value={name} onChange={e => setName(e.target.value)} />
               </FormControl>
   
             { isApplication && 
                 (<FormControl mt={4}>
                     <FormLabel>E-Mail</FormLabel>
-                    <Input readOnly={true} value={application.email}  />
+                    <Input readOnly={true} value={application.email} disabled />
+                </FormControl>)
+            }
+
+            { isApplication && 
+                (<FormControl mt={4}>
+                    <FormLabel>Team</FormLabel>
+                    <Input readOnly={true} value={application.team.name} disabled />
                 </FormControl>)
             }
 
